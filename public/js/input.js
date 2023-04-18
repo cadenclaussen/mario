768bbddaa1d44dad0a65112182c0af0d6ff4109d
Caden Claussen <cadenclaussen@Cadens-Mac-mini.local>
2023-04-18 15:35:52 -0700

    Inital Commit

---
+++ public/js/input.js
@@ -0,0 +1,27 @@
+import Keyboard from './KeyboardState.js';
+
+export function setupKeyboard(mario) {
+    const input = new Keyboard();
+
+    input.addMapping('Space', keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+    });
+
+    input.addMapping('KeyW', keyState => {
+        mario.turbo(keyState);
+    });
+
+    input.addMapping('ArrowRight', keyState => {
+        mario.go.dir += keyState ? 1 : -1;
+    });
+
+    input.addMapping('ArrowLeft', keyState => {
+        mario.go.dir += keyState ? -1 : 1;
+    });
+
+    return input;
+}
---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+import Camera from './Camera.js';
+import Timer from './Timer.js';
+import {loadLevel} from './loaders/level.js';
+import {loadMario} from './entities/Mario.js';
+import { loadGoomba } from './entities/Goomba.js';
+import {setupKeyboard} from './input.js';
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+    loadMario(),
+    loadGoomba(),
+    loadLevel('1-1'),
+])
+.then(([createMario, createGoomba, level]) => {
+    const camera = new Camera();
+    window.camera = camera;
+
+    const mario = createMario();
+    mario.pos.set(64, 64);
+
+    const goomba = createGoomba();
+    // goomba.pos.x = 220;
+
+    level.entities.add(mario);
+    // level.entities.add(goomba);
+
+    const input = setupKeyboard(mario);
+    input.listenTo(window);
+
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
+        level.update(deltaTime);
+
+        if (mario.pos.x > 100) {
+            camera.pos.x = mario.pos.x - 100;
+        }
+
+        level.comp.draw(context, camera);
+    }
+
+    timer.start();
+});
